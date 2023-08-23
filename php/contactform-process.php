a8955ed93fe1517b91f77c8af5905a9ed5fcdae3
feyi-tech <jinminetics@gmail.com>
2023-08-23 05:04:30 +0100

    jquery validator added 3

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -1,7 +1,7 @@
 <?php
 require_once('recaptchalib.php');
 
-$env = parse_ini_file('.env');
+$env = parse_ini_file('../.env');
 $recaptchaCode = $_POST['g-recaptcha-response'];
 
 //echo "<p>ENV_TEST: ".$env["ENV_TEST"]."</p><br/>";
@@ -27,57 +27,61 @@ function clean_string($string) {
 
 if (empty($_POST["name"])) {
     $errorMSG = "Name is required ";
+
+} else if(strlen($_POST["name"]) > 60) {
+    $errorMSG = "Name is too long ";
+
 } else {
     $name = clean_string($_POST["name"]);
 }
 
 if (empty($_POST["email"])) {
     $errorMSG = "Email is required ";
+    
+} else if(strlen($_POST["email"]) > 60) {
+    $errorMSG = "Email is too long ";
+
 } else {
     $email = clean_string($_POST["email"]);
 }
 
 if (empty($_POST["message"])) {
     $errorMSG = "Message is required ";
+    
+} else if(strlen($_POST["message"]) > 500) {
+    $errorMSG = "Message is too long ";
+
 } else {
     $message = clean_string($_POST["message"]);
 }
 
-if (empty($_POST["terms"])) {
-    $errorMSG = "Terms is required ";
-} else {
-    $terms = clean_string($_POST["terms"]);
-}
-
-$EmailTo = "hello@eventgum.com";
-$Subject = "New message from EventGum landing page";
-
-// prepare email body text
-$Body = "";
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
-$Body .= "Terms: ";
-$Body .= $terms;
-$Body .= "\n";
-
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
-
 // redirect to success page
-if ($success && $errorMSG == ""){
-   echo "success";
-}else{
-    if($errorMSG == ""){
-        echo "Something went wrong :(";
+if ($errorMSG == "") {
+    $EmailTo = "hello@eventgum.com";
+    $Subject = "New message from EventGum landing page";
+
+    // prepare email body text
+    $Body = "";
+    $Body .= "Name: ";
+    $Body .= $name;
+    $Body .= "\n";
+    $Body .= "Email: ";
+    $Body .= $email;
+    $Body .= "\n";
+    $Body .= "Message: ";
+    $Body .= $message;
+    $Body .= "\n";
+
+    // send email
+    $success = mail($EmailTo, $Subject, $Body, "From:".$email);
+    if($success) {
+        echo "success";
+
     } else {
-        echo $errorMSG;
+        echo "Something went wrong :(";
     }
+
+} else{
+    echo $errorMSG;
 }
 ?>
